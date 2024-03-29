a8d39376bb32915f76d9b8b53e9dac88a0662658
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-03-29 18:41:54 +0530

    Refactor routing and add PostDetails page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+
 import {Routes,  Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import PostDetails from './pages/PostDetails'
 const App = () => {
   return (
     <div>
-      <Navbar/>
+  
       <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
+      <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
       </Routes>
-      <Footer/>
+     
     </div>
   )
 }
