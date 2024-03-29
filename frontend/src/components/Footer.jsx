fd834deda441e0f22254821f86d008632b27cf0f
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-03-29 16:34:47 +0530

    Update function to handle edge cases***

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
 <div className="mt-8 w-full bg-black px-8 md:px-[300px] flex md:flex-row flex-col space-y-6 md:space-y-0 items-start md:justify-between text-sm md:text-md py-8 ">
        <div className="flex flex-col text-white">
-         <p>Featured Blogs</p>
+         <p>Featured solutions</p>
          <p>Most viewed</p>
          <p>Readers Choice</p>
        </div>
