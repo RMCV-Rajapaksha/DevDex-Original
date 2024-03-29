33c55a6d49aac875b1af84c1cbcebdc9bf463316
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-03-29 18:41:47 +0530

    Add login and register pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { BsSearch } from 'react-icons/bs'
 
 
 const Navbar = () => {
-  const user =true
+  const user =false
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
       <h1 className='text-lg md:text-x1  font-extrabold '>
@@ -15,7 +15,7 @@ const Navbar = () => {
       </div>
       <div className='flex items-center justify-center space-x-2 md:space-x-4'>
         {user? <h3><Link to="/write">Write</Link></h3> :<h3><Link to="/login">Login</Link></h3>}
-        {user? <h3><Link to="/">Profile</Link></h3> :<h3><Link to="/login">Register</Link></h3>}
+        {user? <h3><Link to="/">Profile</Link></h3> :<h3><Link to="/register">Register</Link></h3>}
       </div>
     </div>
   )
