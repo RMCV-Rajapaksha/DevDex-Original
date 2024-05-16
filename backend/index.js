b392f4ee5b7e28ca98382d7723f2feeb59660971
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-05-17 00:26:39 +0530

    feat: Add MongoDB connection and environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
 const express=require('express');
 const app=express();
-
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const authRoute=require('./routes/auth');
 //database connection
 const connectDB=async()=>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/employeeDB',{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('Database connected successfully');
     }catch(err){
         console.log(err);
     }
 }
 
-app.listen(5000,()=>{
-console.log('Server is running on port 5000');
+//middleware
+dotenv.config()
+app.use(express.json())
+app.use("/api/auth",authRoute)
+
+
+app.listen(process.env.PORT,()=>{
+    connectDB();
+console.log('Server is running on port '+process.env.PORT);
 });
