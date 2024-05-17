dfc4d0360fafc215d6546202618db4fcb4d4c026
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-05-17 22:25:59 +0530

    feat: Add routes for comments

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,25 +2,27 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { route } = require('./auth');
+const bcrypt = require('bcrypt');
 const Post = require('../models/Post');
 const Comment = require('../models/Comments');
 
 
+
 //create
-router.post("/create",async(req,res)=>{
+router.post('/create',async (req,res)=>{
     try{
         const newPost=new Post(req.body)
         const savedPost=await newPost.save()
         res.status(200).json(savedPost)
     }
     catch(err){
-        res.status(200).json(err)
+        res.status(500).json(err)
     }
 })
 //update
 router.put('/:id', async (req, res) => {
     try{
-       
+        
         const updatedUser=await Post.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
         res.status(200).json(updatedUser)
 
@@ -36,7 +38,6 @@ router.put('/:id', async (req, res) => {
 router.delete("/:id",async (req,res)=>{
     try{
         await Post.findByIdAndDelete(req.params.id)
-       
         res.status(200).json("Post has been deleted!")
     }
     catch(err){
@@ -48,11 +49,10 @@ router.delete("/:id",async (req,res)=>{
 
 
 
-//get post details
+//get post details 
 router.get('/:id', async (req, res) => {
     try{
     const post = await Post.findById(req.params.id)
-
     res.status(200).json(post)
     }
     catch(err){
@@ -60,12 +60,15 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-
-//get all posts
+//get posts
 router.get('/', async (req, res) => {
+    const query =req.query
+    console.log(query)
     try{
-    const posts= await Post.find()
-
+        const searchFilter ={
+            title:{$regex:query.search,$options:'i'}  //option is for upper  and lower case
+        }
+    const posts = await Post.find(query.search?searchFilter:null)
     res.status(200).json(posts)
     }
     catch(err){
@@ -74,12 +77,23 @@ router.get('/', async (req, res) => {
 })
 
 
-//get user posts
-router.get('/:userId', async (req, res) => {
-    try{
-    const posts= await Post.find({userId:req.params.userId})
 
+//get user posts
+router.get('/user/:userId', async (req, res) => {
+    try{
+    const posts = await Post.find({userId:req.params.userId})
     res.status(200).json(posts)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//search posts
+router.get('/search/:prompt',async(req,res)=>{
+    try{
+
+
     }
     catch(err){
         res.status(500).json(err)
