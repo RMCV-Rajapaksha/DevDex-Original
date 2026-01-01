f78fdcce7b2783ac46639d04c9fdac3b588866ca
RMCV-Rajapaksha <sch.chamara@gmail.com>
2025-12-31 23:10:17 -0800

    feat: Update API URLs to point to the production backend

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,4 @@
 
-export const URL = 'http://localhost:5000'
-export const IF = "http://localhost:5000/images/"
+// export const URL = 'http://localhost:5000'
+export const URL = 'https://dev-dex-original-backend-px6qpj658.vercel.app'
+export const IF = "https://dev-dex-original-backend-px6qpj658.vercel.app/images/"
