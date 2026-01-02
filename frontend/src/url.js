f5b67aeaaec2cc4a667b373fe98296d1c9b61b4c
RMCV-Rajapaksha <sch.chamara@gmail.com>
2026-01-01 17:41:26 -0800

    Refactor code structure for improved readability and maintainability

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,4 +1,5 @@
 
 // export const URL = 'http://localhost:5000'
+// export const IF = "http://localhost:5000/images/"
 export const URL = 'https://dev-dex-original-backend-px6qpj658.vercel.app'
 export const IF = "https://dev-dex-original-backend-px6qpj658.vercel.app/images/"
